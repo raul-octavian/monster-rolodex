da9151ade95e6482c2f04b2afb207e32cf15c0db
raul-octavian <alburaul@gmail.com>
2022-07-10 10:43:25 +0200

    update the state.name with an object

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import { Component } from 'react';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      name: 'Raul',
+    };
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            hello {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => { this.setState({ name: 'Raul-Octavian' }) }}>Complete name</button>
         </header>
       </div>
     );
