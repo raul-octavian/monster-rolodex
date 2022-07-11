491a3e64d88616120bd972d2c79f719bbacae6e3
raul-octavian <alburaul@gmail.com>
2022-07-11 12:17:49 +0200

    the filter is implemented and the component is optimised

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -24,11 +25,35 @@ class App extends Component {
       )
   }
 
+  // define a function for search, defining it optimizes the component
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase()
+    this.setState(() => {
+      // return { monsters: state.monsters.filter(monster => monster.name.includes(event.target.value)) }
+      return { searchField }
+    })
+  }
+
   render() {
+
+    //extract useful variables
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField)
+    })
 
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange} />
+
+        {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>
         })}
       </div>
