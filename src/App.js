86a787cb3dc4c2425c21cd1a7e328ad569bc7c80
raul-octavian <alburaul@gmail.com>
2022-07-11 22:03:09 +0200

    app.js is fully set with useState and useEffect, for fetching and feltering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Component } from 'react';
 import CardList from './components/Card-list/Card-list';
 import SearchBox from './components/Search-box/Search-box';
 
 
+
+
 const App = () => {
+
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters)
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase()
+    setSearchField(searchFieldString)
+  }
+
+  // useEffect is a hook that preforms side effects if the dependencies(second parameter-array) change, if second array is not provided the useEffect will run on each render, is an empty array is given it will run on mount, else it will watch the dependencies provided and it will run when they change
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users)
+      )
+  }, [])
+
+  // filter the monsters array if searchField or monsters array change
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField)
+    })
+    setFilteredMonsters(newFilteredMonsters)
+  }, [searchField, monsters])
+
+
   return (
     <div className="App">
       <h1 className='app--title'>My monster Rolodex</h1>
-      {/* <SearchBox className='search-box' placeholder='search monsters' onChangeHandler={onSearchChange} />
+      <SearchBox
+        className='search-box'
+        placeholder='search monsters'
+        onChangeHandler={onSearchChange} />
 
-      <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
